f4674565ae9b6f5f58e5f3ea46c7c182fae4b343
AlexanderHoogsteyn <hoogsteyna@gmail.com>
2022-08-22 16:26:30 +0200

    - Wrapped iterative method in function - Brought in line with 6 month presentation formulation - Implemented elestic electricity price Known bugs: - Does not converge...

--- ETS-Model.jl
+++ ETS-Model.jl
@@ -17,9 +17,14 @@ using Statistics
 using Distributions, Random
 # If there exist different carbon reducing technologies with different CAPEX OPEX (efficiencies) what is an optimal policy
 
-
+# Read ETS data
+data = YAML.load_file(joinpath(@__DIR__, "data_assumptions_ets.yaml"));
+data_agents = YAML.load_file(joinpath(@__DIR__, "data_assumptions.yaml"));
+routes = data_agents["SteelRoutes"]
+properties = YAML.load_file(joinpath(@__DIR__, "data_properties.yaml"));
 
 # Parameters/variables ETS 
+ETS = Dict()
 function define_ETS_parameters!(ETS::Dict,data::Dict)
     # LRF 2017 - 2021, no Brexit, expansion of scope (aviation, maritime) or Green Deal
     ETS["LRF"] = zeros(data["nyears"],1);
@@ -39,8 +44,10 @@ function define_ETS_parameters!(ETS::Dict,data::Dict)
 
     return ETS
 end
+define_ETS_parameters!(ETS, data)
 
 # Stochastic optimization parameters
+stoch = Dict()
 function define_stoch_parameters!(stoch::Dict,data::Dict)
     nsamples = data["nsamples"]
     avg = data["MAC"]
@@ -49,11 +56,15 @@ function define_stoch_parameters!(stoch::Dict,data::Dict)
     if data["use_cvar"]
         d = Normal(avg,std)
         nselect = floor(Int,data["cvar"]*nsamples)
-        SO["MAC"] = sort(rand(d,nsamples), rev=true)[1:nselect]
-        SO["nsamples"] = size(SO["MAC"])[1]
+        stoch["MAC"] = sort(rand(d,nsamples), rev=true)[1:nselect]
+        stoch["nsamples"] = size(SO["MAC"])[1]
+    else
+        stoch["MAC"] = avg
+        stoch["nsamples"] = 1
     end
     return stoch
 end
+define_stoch_parameters!(stoch,data)
 
 function define_sets_parameters!(mod::Model, data::Dict, stoch::Dict) 
     # Define sets
@@ -82,11 +93,11 @@ function define_sets_parameters!(mod::Model, data::Dict, stoch::Dict)
     # Define variables
     mod.ext[:variables] = Dict() 
     buy = mod.ext[:variables][:buy] =  @variable(mod, [y=Y], lower_bound=0, base_name="allowances bougth")
-    a = mod.ext[:variables][:a] =  @variable(mod, [y=Y], lower_bound=0, base_name="abatement")
+    a = mod.ext[:variables][:a] =  @variable(mod, [y=Y], lower_bound=0, base_name="abatement") # Mton CO2
 
     # Define expressions
     mod.ext[:expressions] = Dict()
-    bank = mod.ext[:expressions][:bank] = @expression(mod, [y=Y], sum(ETS["S"][1:y])+sum(a[1:y])-sum(E[1:y]))
+    mod.ext[:expressions][:bank] = @expression(mod, [y=Y], sum(buy[1:y])+sum(a[1:y])-sum(E[1:y]))
     mod.ext[:expressions][:netto_emiss] = @expression(mod, [y=Y], E[y]-a[y])
 
     MAC = stoch["MAC"] # Marginal abatement cost
@@ -120,7 +131,7 @@ function build_agent!(agent::Model,data::Dict,route::Dict,stoch::Dict)
 
     # define parameters 
     agent.ext[:parameters] = Dict()
-    agent.ext[:parameters][:λ] = zeros(data["nyears"],1)
+    agent.ext[:parameters][:λ_ets] = zeros(data["nyears"],1)
     agent.ext[:parameters][:A] = ones(data["nyears"],1)
     agent.ext[:parameters][:I] = ones(data["nyears"],1)
     for y in 4:data["nyears"]
@@ -129,9 +140,10 @@ function build_agent!(agent::Model,data::Dict,route::Dict,stoch::Dict)
     end
     A = agent.ext[:parameters][:A]
     I = agent.ext[:parameters][:I]
-    λ = agent.ext[:parameters][:λ]
+    λ_ets = agent.ext[:parameters][:λ_ets]
     CAPEX = agent.ext[:parameters][:CAPEX] = data["CAPEX"] # €/MW
-    OPEX = agent.ext[:parameters][:OPEX] = data["p_elek"]/data["efficiency_PEM"]*route["Needs"]["Hydrogen"]/1.850 # €/ton CO2 abated
+    η = agent.ext[:parameters][:η] = data["efficiency_PEM"]
+    f = agent.ext[:parameters][:f_steel] = data["f_steel"]
 
     # Define variables
     agent.ext[:variables] = Dict() 
@@ -140,20 +152,19 @@ function build_agent!(agent::Model,data::Dict,route::Dict,stoch::Dict)
 
     # Define expressions
     agent.ext[:expressions] = Dict()
-    hydrogen = agent.ext[:expressions][:hydrogen] = @expression(agent, [y=Y], g[y]/data["efficiency_PEM"]) # MWh hydrogen
-    abatement_Mton = agent.ext[:expressions][:abatement_Mton] = @expression(agent, [y=Y], g[y]*data["efficiency_PEM"]/route["Needs"]["Hydrogen"]*1.850*1e-6) # Mton CO2
-    abatement_ton = agent.ext[:expressions][:abatement_ton] = @expression(agent, [y=Y], g[y]*data["efficiency_PEM"]/route["Needs"]["Hydrogen"]*1.850) # ton CO2
+    hydrogen = agent.ext[:expressions][:hydrogen] = @expression(agent, [y=Y], g[y]/η) # MWh hydrogen
+    abatement_Mton = agent.ext[:expressions][:abatement_Mton] = @expression(agent, [y=Y], g[y]*η/route["Needs"]["Hydrogen"]*1.850*1e-6) # Mton CO2
+    abatement_ton = agent.ext[:expressions][:abatement_ton] = @expression(agent, [y=Y], g[y]*η/route["Needs"]["Hydrogen"]*1.850) # ton CO2
+    λ_elec = agent.ext[:expressions][:λ_elec] = @expression(agent, [y=Y], data["p_elek"]*I[y] + g[y]*1/10000*I[y] ) # EUR/MWh
 
-    CAPEX = data["CAPEX"] # €/MW
-    OPEX = data["p_elek"]/data["efficiency_PEM"]*route["Needs"]["Hydrogen"]/1.850 # €/ton CO2 abated
     # Objective
-    agent.ext[:objective] = @objective(agent, Min,sum(sum((CAPEX*cap[y] + OPEX*g[y]*I[y])*A[y] - λ[y]*abatement_ton[y]*A[y] for y in Y) for s in S)/stoch["nsamples"])
+    agent.ext[:objective] = @objective(agent, Min,sum(sum((CAPEX*cap[y] + λ_elec[y]*g[y]/η - λ_ets[y]*abatement_ton[y])*A[y] for y in Y) for s in S)/stoch["nsamples"])
   
     # Define constraint
     agent.ext[:constraints] = Dict()
     agent.ext[:constraints][:capacitycons] = @constraint(agent, [y=Y], sum(cap[1:y])*8760 >= g[y])
-    agent.ext[:constraints][:capcons] = @constraint(agent, [y=Y], cap[y]<= 100)
-    agent.ext[:constraints][:maxabatecons] = @constraint(agent, [y=Y], abatement_Mton[y] <= ETS["S"][y]) 
+    #agent.ext[:constraints][:capcons] = @constraint(agent, [y=Y], cap[y]<= 100)
+    agent.ext[:constraints][:maxabatecons] = @constraint(agent, [y=Y], abatement_Mton[y] <= ETS["S"][y]*f)
     return agent
 end
 
@@ -174,31 +185,36 @@ function update_prices!(agent::Model,mod::Model)
     # Fetch variables
     cap = agent.ext[:variables][:cap] # MW electricity
     g = agent.ext[:variables][:g] # MWh electricity
+    a = JuMP.value.(mod.ext[:variables][:a])  # Mton CO2
 
     # Fetch expressions
-    hydrogen = agent.ext[:expressions][:hydrogen] = @expression(agent, [y=Y], g[y]/data["efficiency_PEM"]) # MWh hydrogen
-    abatement_Mton = agent.ext[:expressions][:abatement_Mton] = @expression(agent, [y=Y], g[y]*data["efficiency_PEM"]/route["Needs"]["Hydrogen"]*1.850*1e-6) # Mton CO2
-    abatement_ton = agent.ext[:expressions][:abatement_ton] = @expression(agent, [y=Y], g[y]*data["efficiency_PEM"]/route["Needs"]["Hydrogen"]*1.850) # ton CO2
+    hydrogen = agent.ext[:expressions][:hydrogen]
+    abatement_Mton = agent.ext[:expressions][:abatement_Mton]
+    abatement_ton = agent.ext[:expressions][:abatement_ton]
+    netto_emiss = mod.ext[:expressions][:netto_emiss]
+    λ_elec = agent.ext[:expressions][:λ_elec]
 
     # Fetch Parameters
     A = agent.ext[:parameters][:A]
     I = agent.ext[:parameters][:I]
     CAPEX = agent.ext[:parameters][:CAPEX]
-    OPEX = agent.ext[:parameters][:OPEX]
-    λ = -[shadow_price(mod.ext[:constraints][:buycons][i]) for i in 1:45]./A[:,1]
+    λ_ets = -[shadow_price(mod.ext[:constraints][:buycons][i]) for i in 1:45]./A[:,1]
     e = JuMP.value.(mod.ext[:expressions][:netto_emiss])
+    η = agent.ext[:parameters][:η]
+    f = agent.ext[:parameters][:f_steel]
 
     # Update prices
-    
-    mod.ext[:parameters][:λ] = λ
-    agent.ext[:parameters][:λ] = λ
+    mod.ext[:parameters][:λ] = λ_ets
+    agent.ext[:parameters][:λ] = λ_ets
     
     # Objective
-    agent.ext[:objective] = @objective(agent, Min,sum(sum((CAPEX*cap[y] + OPEX*g[y]*I[y])*A[y] - λ[y]*abatement_ton[y]*A[y] for y in Y) for s in S)/nb_S)
+    agent.ext[:objective] = @objective(agent, Min,sum(sum((CAPEX*cap[y] + λ_elec[y]*g[y]/η- λ_ets[y]*abatement_ton[y])*A[y] for y in Y) for s in S)/nb_S)
 
-    # Add max abatement constraint
-    #agent.ext[:constraints][:maxabatecons] = @constraint(agent, [y=Y], abatement_Mton[y] <= e[y])
-    return λ
+    # Adjust max abatement constraint for change competitive fringe
+    for y in Y
+        set_normalized_rhs(agent.ext[:constraints][:maxabatecons][y], e[y]*f)
+    end
+    return λ_ets
 end    
 
 function update_abatement!(mod::Model, agent::Model)
@@ -212,10 +228,9 @@ function update_abatement!(mod::Model, agent::Model)
 
     # Fetch variables
     buy = mod.ext[:variables][:buy]
-    a = mod.ext[:variables][:a]
+    a = mod.ext[:variables][:a]  # Mton CO2
 
     # Fetch exprsessions
-    bank = mod.ext[:expressions][:bank]
     netto_emiss = mod.ext[:expressions][:netto_emiss]
     abatement_Mton = JuMP.value.(agent.ext[:expressions][:abatement_Mton])
 
@@ -224,6 +239,9 @@ function update_abatement!(mod::Model, agent::Model)
     for y in Y 
         set_normalized_rhs(mod.ext[:constraints][:buycons][y], ETS["S"][y] + abatement_Mton[y])
     end
+
+    # Update bank
+    mod.ext[:expressions][:bank] = @expression(mod, [y=Y], sum(buy[1:y])+sum(a[1:y])-sum(E[1:y]))
     return mod
 end
 
@@ -234,11 +252,6 @@ end
 ##################################################################
 
 function run_ets_model()
-    # Read ETS data
-    data = YAML.load_file(joinpath(@__DIR__, "data_assumptions_ets.yaml"));
-    ETS = Dict()
-    define_ETS_parameters!(ETS,data)
-
     # Initialize model
     mod = Model(optimizer_with_attributes(Gurobi.Optimizer))
     define_sets_parameters!(mod,data)
@@ -253,106 +266,97 @@ function run_ets_model()
     bank_opt = convert(Array, JuMP.value.(mod.ext[:expressions][:bank]))
     emission_opt = mod.ext[:parameters][:e][:,1] - abate_opt 
     supply = ETS["S"][:,1]
-    prices =  [shadow_price(mod.ext[:constraints][:buycons][i]) for i in 1:45]./mod.ext[:parameters][:A][:,1]
+    prices =  -[shadow_price(mod.ext[:constraints][:buycons][i]) for i in 1:45]./mod.ext[:parameters][:A][:,1]
     
     # Print results
-    sol = DataFrame(Y=2019:2063,Supply=supply,Buy=buy_opt, Abate=abate_opt, Emmit=emission_opt,Bank=bank_opt,Price=prices)
+    sol = DataFrame(Y=2019:2063,Supply=supply,Buy=buy_opt, Abate=abate_opt, Emmit=emission_opt,Bank=bank_opt,Price=prices);
     print(sol)
     # Plot results
     plot(Matrix(sol)[:,2:end],labels=permutedims(names(sol[:,2:end])))
+    return sol
 end 
 
 function run_stochastic_ets_model()
-    # Read ETS data
-    data = YAML.load_file(joinpath(@__DIR__, "data_assumptions_ets.yaml"));
-    ETS = Dict()
-    define_ETS_parameters!(ETS, data)
+        # Initialize model
+        mod = Model(optimizer_with_attributes(Gurobi.Optimizer))
+        define_sets_parameters!(mod,data, stoch)
 
-    # Make stochastic
-    stoch = Dict()
-    define_stoch_parameters!(stoch,data)
+        # Solve
+        optimize!(mod)
+        @show objective_value(mod);
 
-    # Initialize model
+        # Fetch solution
+        buy_opt = convert(Array, JuMP.value.(mod.ext[:variables][:buy]))
+        abate_opt = convert(Array, JuMP.value.(mod.ext[:variables][:a]))
+        bank_opt = convert(Array, JuMP.value.(mod.ext[:expressions][:bank]))
+        emission_opt = mod.ext[:parameters][:e][:,1] - abate_opt 
+        supply = ETS["S"][:,1]
+        prices =  -[shadow_price(mod.ext[:constraints][:buycons][i]) for i in 1:45]./mod.ext[:parameters][:A][:,1]
+        
+        # Print results
+        sol = DataFrame(Y=2019:2063,Supply=supply,Buy=buy_opt, Abate=abate_opt, Emmit=emission_opt,Bank=bank_opt,Price=prices);
+        
+        # Plot results
+        plot(Matrix(sol)[:,2:end],labels=permutedims(names(sol[:,2:end])))
+        return sol
+    end
+
+    function run_joint_ets_model_iter()
+    # Initial solve model
     mod = Model(optimizer_with_attributes(Gurobi.Optimizer))
-    define_sets_parameters!(mod,data, stoch)
+    define_sets_parameters!(mod,data)
+        # For one agent which can use H-DRI
+    optimize!(mod);
 
-    # Solve
-    optimize!(mod)
-    @show objective_value(mod);
+    # Set up agent
+    agent = Model(optimizer_with_attributes(Gurobi.Optimizer))
+    route = routes["HDRI-EAF"]
+    build_agent!(agent,data,route)
 
-    # Fetch solution
-    buy_opt = convert(Array, JuMP.value.(mod.ext[:variables][:buy]))
-    abate_opt = convert(Array, JuMP.value.(mod.ext[:variables][:a]))
-    bank_opt = convert(Array, JuMP.value.(mod.ext[:expressions][:bank]))
-    emission_opt = mod.ext[:parameters][:e][:,1] - abate_opt 
-    supply = ETS["S"][:,1]
-    prices =  [shadow_price(mod.ext[:constraints][:buycons][i]) for i in 1:45]./mod.ext[:parameters][:A][:,1]
-    
-    # Print results
-    sol = DataFrame(Y=2019:2063,Supply=supply,Buy=buy_opt, Abate=abate_opt, Emmit=emission_opt,Bank=bank_opt,Price=prices)
-    
-    # Plot results
-    plot(Matrix(sol)[:,2:end],labels=permutedims(names(sol[:,2:end])))
-end
-
-# Read ETS data
-data = YAML.load_file(joinpath(@__DIR__, "data_assumptions_ets.yaml"));
-data_agents = YAML.load_file(joinpath(@__DIR__, "data_assumptions.yaml"));
-routes = data_agents["SteelRoutes"]
-properties = YAML.load_file(joinpath(@__DIR__, "data_properties.yaml"));
-ETS = Dict()
-define_ETS_parameters!(ETS, data)
-
-# Initial solve model
-mod = Model(optimizer_with_attributes(Gurobi.Optimizer))
-define_sets_parameters!(mod,data)
-    # For one agent which can use H-DRI
-optimize!(mod);
-
-# Set up agent
-agent = Model(optimizer_with_attributes(Gurobi.Optimizer))
-route = routes["HDRI-EAF"]
-build_agent!(agent,data,route)
-
-prices = update_prices!(agent,mod);
-optimize!(agent);
-update_abatement!(mod,agent);
-optimize!(mod);
-updated_prices = update_prices!(agent,mod);
-prices = hcat(prices,updated_prices)
-
-δ = sum((prices[:,end-1] - prices[:,end]).^2)
-iter = 1
-
-#Keep solving iteratively
-while δ  > 1e-6
+    prices = update_prices!(agent,mod);
     optimize!(agent);
     update_abatement!(mod,agent);
     optimize!(mod);
     updated_prices = update_prices!(agent,mod);
-    global prices = hcat(prices,updated_prices)
-    global δ = sum((prices[:,end-1] - prices[:,end]).^2)
-    global iter += 1 
-    if iter == 100
-        break
-    end
+    prices = hcat(prices,updated_prices)
+
+    δ = sum((prices[:,end-1] - prices[:,end]).^2)
+    iter = 1
+
+    #Keep solving iteratively
+    while δ  > 1e-6
+        optimize!(agent);
+        update_abatement!(mod,agent);
+        optimize!(mod);
+        updated_prices = update_prices!(agent,mod);
+        prices = hcat(prices,updated_prices)
+        δ = sum((prices[:,end-1] - prices[:,end]).^2)
+        iter += 1 
+        if iter == 100
+            print("Did not converge after " * string(iter) * " runs     δ= " * string(δ) * "\n")
+            print("--------------------------------------------------\n")
+            break
+        end
+        print("Run: " * string(iter) * "     δ= " * string(δ) * "\n")
+        print("--------------------------------------------------\n")
+    end;
+
+    # final optimization to circonvent OptimizeNotCalled()
+    optimize!(mod);
+    optimize!(agent);
+
+    frac_digit = 4
+    buy_opt = round.(convert(Array, JuMP.value.(mod.ext[:variables][:buy])),digits=frac_digit)
+    abate_opt = round.(convert(Array, JuMP.value.(mod.ext[:variables][:a])),digits=frac_digit)
+    bank_opt = round.(convert(Array, JuMP.value.(mod.ext[:expressions][:bank])),digits=frac_digit)
+    emission_opt = round.(mod.ext[:parameters][:e][:,1] - abate_opt ,digits=frac_digit)
+    supply = round.(ETS["S"][:,1],digits=frac_digit)
+
+    cap_opt = round.(convert(Array, JuMP.value.(agent.ext[:variables][:cap])),digits=frac_digit) # MW electricity
+    g_opt = round.(convert(Array, JuMP.value.(agent.ext[:variables][:g])),digits=frac_digit) # MWh electricity
+    abatement_Mton = round.(convert(Array, JuMP.value.(agent.ext[:expressions][:abatement_Mton])),digits=frac_digit)
+
+    # Print results
+    sol = DataFrame(Y=2019:2063,Buy=buy_opt, Abate=abate_opt, Emmit=emission_opt,Bank=bank_opt,InitPrice=prices[:,1], PriceUpdate=prices[:,end], Capacity=cap_opt,Agent=abatement_Mton);
+    return sol
 end;
-
-# final optimization to circonvent OptimizeNotCalled()
-optimize!(mod);
-optimize!(agent);
-
-frac_digit = 4
-buy_opt = round.(convert(Array, JuMP.value.(mod.ext[:variables][:buy])),digits=frac_digit)
-abate_opt = round.(convert(Array, JuMP.value.(mod.ext[:variables][:a])),digits=frac_digit)
-bank_opt = round.(convert(Array, JuMP.value.(mod.ext[:expressions][:bank])),digits=frac_digit)
-emission_opt = round.(mod.ext[:parameters][:e][:,1] - abate_opt ,digits=frac_digit)
-supply = round.(ETS["S"][:,1],digits=frac_digit)
-
-cap_opt = round.(convert(Array, JuMP.value.(agent.ext[:variables][:cap])),digits=frac_digit) # MW electricity
-g_opt = round.(convert(Array, JuMP.value.(agent.ext[:variables][:g])),digits=frac_digit) # MWh electricity
-abatement_Mton = round.(convert(Array, JuMP.value.(agent.ext[:expressions][:abatement_Mton])),digits=frac_digit)
-
-# Print results
-sol = DataFrame(Y=2019:2063,Buy=buy_opt, Abate=abate_opt, Emmit=emission_opt,Bank=bank_opt,InitPrice=prices[:,1], PriceUpdate=prices[:,end], Capacity=cap_opt,Agent=abatement_Mton)
-print(sol)
